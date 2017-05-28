074535986704239063740499b46a2ba565337ec1
alejoescobar <alejoescobac@gmail.com>
2017-05-28 12:17:34 -0500

    Server static page

--- app.js
+++ app.js
@@ -3,13 +3,7 @@ const app = express()
 const os = require('os')
 const path = require('path')
 
-<<<<<<< HEAD
-app.get('/', function (req, res) {
-  res.send('Hello World Angel!')
-})
-=======
 app.use('/', express.static('public'))
->>>>>>> cool-landing
 
 app.get('/machine-info', function(req, res) {
   res.send(os.networkInterfaces())

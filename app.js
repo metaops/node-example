3be4772aee4b4029aea92c54d4728c9b8c123c66
Simon Soriano <simon0191@gmail.com>
2017-05-28 10:33:09 -0500

    Add machine info endpoint

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const os = require('os')
 
 app.get('/', function (req, res) {
-  res.send('Hello World Julian! '+ req.hostname)
+  res.send('Hello World Julian!')
+})
+
+app.get('/machine-info', function(req, res) {
+  res.send(os.networkInterfaces())
 })
 
 var port = process.env.PORT || 3000;
